dabbe520ef0a4a4ba13f3d335854c96f4cf7f9ed
Riccardo Tartaglia <tartaglia.riccardo@gmail.com>
2021-01-22 17:12:26 +0100

    add error mex for empty transaction in addSegment

--- src/inspector.js
+++ src/inspector.js
@@ -1,131 +1,135 @@
-const Transaction = require('./lib/transaction.js')
-const Segment = require('./lib/segment.js')
-const Transport = require('./lib/transport')
-const Instrumentation = require('./lib/instrumentation')
-const IError = require('./lib/error')
+const Transaction = require("./lib/transaction.js");
+const Segment = require("./lib/segment.js");
+const Transport = require("./lib/transport");
+const Instrumentation = require("./lib/instrumentation");
+const IError = require("./lib/error");
 
 class Inspector {
-
-  constructor (conf = {}) {
+  constructor(conf = {}) {
     this._conf = {
-      url: 'ingest.inspector.dev',
-      ingestionKey: '',
+      url: "ingest.inspector.dev",
+      ingestionKey: "",
       enabled: true,
-      version: '1.7.6',
+      version: "1.7.6",
       autoWiring: true,
       modules: [],
       maxEntries: 100,
-      ...conf
-    }
-    this._transaction = null
-    this.transport = new Transport(this._conf)
+      ...conf,
+    };
+    this._transaction = null;
+    this.transport = new Transport(this._conf);
 
     // patch modules with instrumentations
-    Instrumentation.init(this)
+    Instrumentation.init(this);
 
-    process.on('uncaughtException', async (err, origin) => {
+    process.on("uncaughtException", async (err, origin) => {
       if (!this.isRecording()) {
-        this.startTransaction('Error')
+        this.startTransaction("Error");
       }
-      await this.reportException(err)
-    })
+      await this.reportException(err);
+    });
 
-    process.on('unhandledRejection', async (err, origin) => {
+    process.on("unhandledRejection", async (err, origin) => {
       if (!this.isRecording()) {
-        this.startTransaction('Error')
+        this.startTransaction("Error");
       }
-      await this.reportException(err)
-    })
+      await this.reportException(err);
+    });
 
-    process.on('beforeExit', (code) => {
+    process.on("beforeExit", (code) => {
       if (this.isRecording()) {
-        this.flush()
+        this.flush();
       }
-    })
+    });
   }
 
-  startTransaction (name) {
-    this._transaction = new Transaction(name)
-    this._transaction.start()
+  startTransaction(name) {
+    this._transaction = new Transaction(name);
+    this._transaction.start();
 
-    this.addEntries(this._transaction)
+    this.addEntries(this._transaction);
 
-    return this._transaction
+    return this._transaction;
   }
 
-  currentTransaction () {
-    return this._transaction
+  currentTransaction() {
+    return this._transaction;
   }
 
-  isRecording () {
-    return !!this._transaction
+  isRecording() {
+    return !!this._transaction;
   }
 
-  startSegment (type, label = null) {
-    const segment = new Segment(this._transaction, type, label)
-    segment.start()
+  startSegment(type, label = null) {
+    if (!this.isRecording()) {
+      throw new Error("No transaction active found");
+    }
+    const segment = new Segment(this._transaction, type, label);
+    segment.start();
 
-    this.addEntries(segment)
+    this.addEntries(segment);
 
-    return segment
+    return segment;
   }
 
-  async addSegment (fn, type, label = null, throwE = false) {
-    const segment = this.startSegment(type, label)
+  async addSegment(fn, type, label = null, throwE = false) {
+    const segment = this.startSegment(type, label);
     try {
-      if (fn[Symbol.toStringTag] === 'AsyncFunction') {
-        return await fn(segment)
+      if (fn[Symbol.toStringTag] === "AsyncFunction") {
+        return await fn(segment);
       } else {
-        return fn(segment)
+        return fn(segment);
       }
     } catch (e) {
-      await this.reportException(e)
+      await this.reportException(e);
       if (throwE) {
-        throw e
+        throw e;
       }
     } finally {
-      segment.end()
+      segment.end();
     }
   }
 
-  async reportException (error) {
-    const segment = this.startSegment('exception', error.message)
-    const e = new IError(error, this._transaction)
-    await e.populateError()
-    this.addEntries(e)
-    segment.end()
+  async reportException(error) {
+    const segment = this.startSegment("exception", error.message);
+    const e = new IError(error, this._transaction);
+    await e.populateError();
+    this.addEntries(e);
+    segment.end();
   }
 
-  addEntries (data) {
-    if (Array.isArray(this.transport.queue) && this.transport.queue.length < this._conf.maxEntries) {
+  addEntries(data) {
+    if (
+      Array.isArray(this.transport.queue) &&
+      this.transport.queue.length < this._conf.maxEntries
+    ) {
       if (Array.isArray(data)) {
-        data.forEach(item => {
-          this.transport.addEntry(item)
-        })
+        data.forEach((item) => {
+          this.transport.addEntry(item);
+        });
       } else {
-        this.transport.addEntry(data)
+        this.transport.addEntry(data);
       }
     }
-    return this
+    return this;
   }
 
-  flush () {
+  flush() {
     if (!this._conf.enabled || !this.isRecording()) {
-      return
+      return;
     }
 
     if (!this._transaction.isEnded()) {
-      this._transaction.end()
+      this._transaction.end();
     }
 
-    this.transport.flush()
-    this._transaction = null
+    this.transport.flush();
+    this._transaction = null;
   }
 
-  get conf () {
-    return this._conf
+  get conf() {
+    return this._conf;
   }
-
 }
 
-module.exports = Inspector
+module.exports = Inspector;
