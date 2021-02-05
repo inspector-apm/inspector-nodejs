af24185e151571e72c43c193df528ec2f5bd5536
Riccardo Tartaglia <tartaglia.riccardo@gmail.com>
2021-02-05 12:23:43 +0100

    added mssql support

--- src/lib/instrumentation.js
+++ src/lib/instrumentation.js
@@ -1,59 +1,65 @@
-const Hook = require('require-in-the-middle')
-const path = require('path')
+const Hook = require("require-in-the-middle");
+const path = require("path");
 
 const _MODULES = {
-  'mysql2': (agent) => {
-    Hook(['mysql2'], function (exports, name, basedir) {
-      const version = require(path.join(basedir, 'package.json')).version
-      return require('../modules/mysql2.js')(exports, agent, version)
-    })
+  mysql2: (agent) => {
+    Hook(["mysql2"], function (exports, name, basedir) {
+      const version = require(path.join(basedir, "package.json")).version;
+      return require("../modules/mysql2.js")(exports, agent, version);
+    });
   },
-  'express': (agent) => {
-    Hook(['express'], function (exports, name, basedir) {
+  mssql: (agent) => {
+    Hook(["mssql"], function (exports, name, basedir) {
+      const version = require(path.join(basedir, "package.json")).version;
+      return require("../modules/mssql.js")(exports, agent, version);
+    });
+  },
+  express: (agent) => {
+    Hook(["express"], function (exports, name, basedir) {
       agent.expressMiddleware = (opts = {}) => {
-        return require('../modules/express.js')(agent, opts)
-      }
-      return exports
-    })
+        return require("../modules/express.js")(agent, opts);
+      };
+      return exports;
+    });
   },
-  'pg': (agent) => {
-    Hook(['pg'], function (exports, name, basedir) {
-      const version = require(path.join(basedir, 'package.json')).version
-      return require('../modules/pg.js')(exports, agent, version)
-    })
+  pg: (agent) => {
+    Hook(["pg"], function (exports, name, basedir) {
+      const version = require(path.join(basedir, "package.json")).version;
+      return require("../modules/pg.js")(exports, agent, version);
+    });
   },
-  'mongodb': (agent) => {
-    Hook(['mongodb'], function (exports, name, basedir) {
-      const version = require(path.join(basedir, 'package.json')).version
-      return require('../modules/mongodb.js')(exports, agent, version)
-    })
+  mongodb: (agent) => {
+    Hook(["mongodb"], function (exports, name, basedir) {
+      const version = require(path.join(basedir, "package.json")).version;
+      return require("../modules/mongodb.js")(exports, agent, version);
+    });
   },
-  'knex': (agent) => {
-    Hook(['knex'], function (exports, name, basedir) {
-      const version = require(path.join(basedir, 'package.json')).version
-      return require('../modules/knex.js')(exports, agent, version)
-    })
+  knex: (agent) => {
+    Hook(["knex"], function (exports, name, basedir) {
+      const version = require(path.join(basedir, "package.json")).version;
+      return require("../modules/knex.js")(exports, agent, version);
+    });
   },
-}
+};
 
 class Instrumentation {
-  init (agent) {
-    if(agent._conf.autoWiring){
-       Object.keys(_MODULES).map(module => {
-         this.patch(module, agent)
-       })
+  init(agent) {
+    if (agent._conf.autoWiring) {
+      Object.keys(_MODULES).map((module) => {
+        this.patch(module, agent);
+      });
     } else {
-      agent._conf.modules.forEach(module => this.patch(module, agent))
+      agent._conf.modules.forEach((module) => this.patch(module, agent));
     }
   }
 
-  patch (name, agent) {
+  patch(name, agent) {
     if (Object.keys(_MODULES).indexOf(name) === -1) {
-      throw new Error(`Module ${name} not supported`)
+      throw new Error(`Module ${name} not supported`);
     }
-    _MODULES[name](agent)
-    return this
+    _MODULES[name](agent);
+    return this;
   }
 }
 
-module.exports = new Instrumentation()
+module.exports = new Instrumentation();
