a54ec60fcf57253ccbf32cf44d04c5fc761b806e
Valerio Barbera <ilvalerione@gmail.com>
2021-09-28 17:12:13 +0200

    update

--- src/inspector.js
+++ src/inspector.js
@@ -10,7 +10,7 @@ class Inspector {
       url: "ingest.inspector.dev",
       ingestionKey: "",
       enabled: true,
-      version: "1.9.6",
+      version: "1.9.7",
       autoWiring: true,
       modules: [],
       maxEntries: 100,
