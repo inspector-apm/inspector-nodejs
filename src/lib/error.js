b3c92ec1f69371e17bf4a12ffde4f649cee315e9
Valerio Barbera <ilvalerione@gmail.com>
2023-07-13 12:00:03 +0200

    fix populate error

--- src/lib/error.js
+++ src/lib/error.js
@@ -73,7 +73,7 @@ class IError {
 
     for (const [index, line] of rawStack.entries()) {
       let lineObj = stacktrace.stackLineParser(line)
-      lineObj.stack = await stacktrace.getCodeOfStackElement(lineObj)
+      lineObj.code = await stacktrace.getCodeOfStackElement(lineObj)
 
       // Populate the top level object
       if (index === 0) {
